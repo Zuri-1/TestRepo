dbf32187a371db4dc1126e0173b2d886f0827191
Zuri-1 <sujaytuladhar23@augustana.edu>
2024-09-26 11:44:22 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,11 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello bird world!");
+
+        Chicken chicken1 = new Chicken();
+
+        chicken1.dance();
     }
 }
